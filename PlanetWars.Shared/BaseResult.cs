f141f6862e3f0b0eb5598f7de21ac3bf560a85c2
eonarheim <erik.onarheim@gmail.com>
2016-01-23 13:10:53 -0600

    Create PlantWars.Shared

---
+++ PlanetWars.Shared/BaseResult.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanetWars.Shared
+{
+    public class BaseResult
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+
+        public static BaseResult Succeed()
+        {
+            return new BaseResult()
+            {
+                Success = true,
+                Message = "Success"
+            };
+        }
+
+        public static BaseResult Fail(string message)
+        {
+            return new BaseResult()
+            {
+                Success = false,
+                Message = message
+            };
+        }
+    }
+}
