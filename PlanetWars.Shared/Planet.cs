f90f8672ef6c5b34af22c3248cf62dc9ce7d7a0d
eonarheim <erik.onarheim@gmail.com>
2016-01-23 14:02:04 -0600

    Move public DTOs to Shared

--- PlanetWars.Shared/Planet.cs
+++ PlanetWars.Shared/Planet.cs
@@ -1,22 +1,16 @@
-﻿using PlanetWars.Shared;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PlanetWars.Server
+namespace PlanetWars.Shared
 {
     public class Planet
-    {        
+    {
         public int Id { get; set; }
         public int NumberOfShips { get; set; }
         public Point Position { get; set; }
-
-        /// <summary>
-        /// Owner string as guid, neutral player is zero guid
-        /// </summary>
-        public string Owner { get; set; } = Guid.Empty.ToString();
-
+        public int OwnerId { get; set; } = -1;
     }
 }
