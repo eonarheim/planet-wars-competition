76179d832c5302344ba259d40b44001a4e587827
Kamran Ayub <kamran.ayub@gmail.com>
2016-01-24 15:29:48 -0600

    Fix web api contract resolver

--- PlanetWars/App_Start/WebApiConfig.cs
+++ PlanetWars/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using Newtonsoft.Json.Serialization;
 
 namespace PlanetWars
 {
@@ -10,6 +11,7 @@ namespace PlanetWars
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
 
             // Web API routes
             config.MapHttpAttributeRoutes();
